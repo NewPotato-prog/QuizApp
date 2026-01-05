2d7816bb767816fbbefe1067374917b644a533ae
NewPotato-prog <pranjal.bijalwan1924@proton.me>
2026-01-06 01:03:40 +0530

    added basic manager

--- question.py
+++ question.py
@@ -1,25 +1,14 @@
-from typing import Any
-
-
 class Question:
     text: str
     type: str # MCQ / One word / numeric?
     answer : str | int | float
     category : str | None# History, Geo, Math etc.
-    options : list[str | int | float] | None
+    options : list[str] | list[int] | list[float] | None
 
-    def __init__(self, text : str, type : str, answer : str, category : str | None= None, options : list[str | int | float] | None = None ) -> None:
+    def __init__(self, text : str, type : str, answer : str, category : str | None= None, options : list[str] | list[int] | list[float] | None = None ) -> None:
         self.text = text
         self.type = type
         self.answer = answer
         self.category = category
         self.options = options
     
-    def toJson(self) -> dict[str, Any]:
-        return {
-            "text" : self.text,
-            "type" : self.type,
-            "answer" : self.answer,
-            "category" : self.category,
-            "options" : self.options
-        }
--- quiz_manager.py
+++ quiz_manager.py
@@ -1,19 +1,67 @@
-from pickle import load
+from pickle import UnpicklingError, dump, load
 from typing import Any
+
+from question import Question
 
 class Manager:
     def __init__(self) -> None:
         self.file_path : str = "questions.bin"
-        self.questions : list[dict[str, Any]] = []
+        self.questions : list[Question] = []
+        
         self.readFile()
     
     def readFile(self):
+        # TODO : Check if file exists
         file = open(self.file_path, "rb")
-        self.questions  : list[dict[str, Any]] = load(file)
+        questions = []
+        try :
+            questions  : Any = load(file)
+        except UnpicklingError or EOFError:
+            print("File is corrupted! Recreating")
+            file.close()
+            file = open(self.file_path, "wb")
+            dump([], file)
+
+        self.questions = questions
+
+    def showQuestions(self):
+        for question in self.questions:
+            print(question.text)
+
+    def writeFile(self):
+        file = open(self.file_path, "wb")
+        dump(self.questions, file)
+    
+    def addQuestion(self):
+        text = input("Enter the question: ")
+        type = input("Enter the type of question (MCQ/One word/Numeric): ")
+        answer = input("Enter the answer: ")
+        category = input("Enter the category: ")
+        options = input("Enter the options (seperated by comma): ")
+        options = options.split(",")
+        question = Question(text, type, answer, category, options)
+        
+        self.questions.append(question)
+        self.writeFile()
 
     def showOptions(self):
-        data = self.readFile()
-        for question in data:
-            print(question["text"])
-            print(question["options"])
-            print()
+        print("CHOOSE AN OPTION")
+        print("1. Show Questions")
+        print("2. Add a Question")
+        print("2. Exit")
+        while True:
+            i = input("Enter your choice: ")
+            if i == "1":
+                self.showQuestions()
+            elif i == "2":
+                self.addQuestion()
+            elif i == "3":
+                break
+            else:
+                print("Invalid Choice")
+                continue
+
+Manager().showOptions()
+
+
+        
